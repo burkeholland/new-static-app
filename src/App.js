7db3d01d630d43fa81929b53e13ded2828b8b41a
Burke Holland <burkeholland@gmail.com>
2021-06-30 13:12:36 -0500

    Fix unused logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="https://i.imgur.com/xf6bG3Z.png" className="App-logo" alt="logo" />
+        <img
+          src="https://i.imgur.com/xf6bG3Z.png"
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
